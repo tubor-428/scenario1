eb5a0aec439febe4abdb450522d6d9014972d250
zhangyiming <zhangyiming@zhangyimingdeMacBook-Pro.local>
2024-06-05 19:26:22 +0800

    tcp server & http request

--- tcpclient/src/main.rs
+++ tcpclient/src/main.rs
@@ -1,3 +1,14 @@
+use std::io::{Read, Write};
+use std::net::TcpStream;
+use std::str;
+
 fn main() {
-    println!("Hello, world!");
+    let mut stream = TcpStream::connect("127.0.0.1:3000").unwrap();
+    stream.write("Hello".as_bytes()).unwrap();
+    let mut buffer = [0; 5];
+    stream.read(&mut buffer).unwrap();
+    println!(
+        "Got response from server: {:?}",
+        str::from_utf8(&buffer).unwrap()
+    );
 }
