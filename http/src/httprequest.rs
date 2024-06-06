650135ec8f78d93dc62c40afea0fe38665035d82
zhangyiming <zhangyiming@zhangyimingdeMacBook-Pro.local>
2024-06-06 19:07:45 +0800

    complete demo httpserver

--- http/src/httprequest.rs
+++ http/src/httprequest.rs
@@ -64,8 +64,9 @@ impl From<String> for HttpRequest {
                 parsed_resource = resource;
             } else if line.contains(":") {
                 let (key, value) = process_header_line(line);
+                parsed_headers.insert(key, value);
             } else if line.len() == 0 {
-                // if none of these
+                // if none of these, treat it as message body
             } else {
                 parsed_msg_body = line;
             }
@@ -121,5 +122,19 @@ mod tests {
     fn test_version_into() {
         let m: Version = "HTTP/1.1".into();
         assert_eq!(m, Version::V1_1);
+    }
+
+    #[test]
+    fn test_read_http() {
+        let s: String = String::from("GET /greeting HTTP/1.1\r\nHost: localhost:3000\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n");
+        let mut headers_expected = HashMap::new();
+        headers_expected.insert("Host".into(), " localhost".into());
+        headers_expected.insert("Accept".into(), " */*".into());
+        headers_expected.insert("User-Agent".into(), " curl/7.64.1".into());
+        let req: HttpRequest = s.into();
+        assert_eq!(Method::Get, req.method);
+        assert_eq!(Version::V1_1, req.version);
+        assert_eq!(Resource::Path("/greeting".to_string()), req.resource);
+        assert_eq!(headers_expected, req.headers);
     }
 }
